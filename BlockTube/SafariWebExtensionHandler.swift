7ad40d38ca97d83bec16457f24e429a0dec7d89d
KC-2001MS <iroiro.work1234@gmail.com>
2023-02-12 14:20:50 +0900

    Added copyright notice

--- BlockTube/SafariWebExtensionHandler.swift
+++ BlockTube/SafariWebExtensionHandler.swift
@@ -3,7 +3,10 @@
 //  BlockTube macOS
 //
 //  Created by 茅根啓介 on 2023/02/10.
-//
+//------------------------------------------
+//  GNU General Public License v3.0
+//  © 2023 Free Software Foundation, Inc.
+//  For more information on the use of the code, please refer to the license
 
 import SafariServices
 import os.log
